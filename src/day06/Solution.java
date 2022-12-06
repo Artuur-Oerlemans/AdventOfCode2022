4c5b207a2b439f8c02c73c9255e6b7d85e6c87a7
Artuur <artuur.oerlemans@gmail.com>
2022-12-06 21:54:16 +0100

    day 6

---
+++ src/day06/Solution.java
@@ -0,0 +1,82 @@
+package day06;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    public static final String ANSI_BOLD = "\033[0;1m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String... input) {
+        var solver = new Solver();
+        String stringData = readData(input[0]);
+
+        System.out.println("\nexecuting part 1");
+        Instant start1 = Instant.now();
+        System.out.format(ANSI_BOLD + "result of part 1: %s\n", solver.part1(stringData) + ANSI_RESET);
+        Instant end1 = Instant.now();
+        System.out.println("Time taken: " + Duration.between(start1, end1) + "\n");
+
+        System.out.println("\nexecuting part 2");
+        Instant start2 = Instant.now();
+        System.out.format(ANSI_BOLD + "result of part 2: %s\n" + ANSI_RESET, solver.part2(stringData));
+        Instant end2 = Instant.now();
+        System.out.println("Time taken: " + Duration.between(start2, end2));
+    }
+
+    private static String readData(String fileName) {
+        System.out.format("Reading file: %s\n", fileName);
+        Path path = Path.of(".", fileName);
+        try{
+            return Files.readString(path);
+        } catch (IOException e) {
+            System.err.format("An exception occurred while reading file '%s' ", fileName);
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Solver {
+    Object part1(String stringData) {
+        return findMarker(stringData, 4);
+    }
+
+    Object part2(String stringData) {
+        return findMarker(stringData, 14);
+    }
+
+    private int findMarker(String stringData, int numberDistinct) {
+        var lines = Helper.stringToCharList(stringData);
+        for(int i = numberDistinct; i < lines.size(); i++) {
+            var set =
+                    new HashSet<>(
+                            lines.subList(i - numberDistinct, i)
+                    );
+
+            if (set.size() == numberDistinct) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+class Helper {
+    public static Set<Character> stringToCharSet(String s) {
+        return new HashSet<>(s.chars().mapToObj(e->(char)e).toList());
+    }
+
+    public static List<Character> stringToCharList(String s) {
+        return new ArrayList<>(s.chars().mapToObj(e->(char)e).toList());
+    }
+}
