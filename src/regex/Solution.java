843dd9e8a2e164ea8ef65aeca7e06da7c46f8956
Artuur <artuur.oerlemans@gmail.com>
2022-12-11 21:46:14 +0100

    regex! This way I can easily extract ints from oddly formatted lines.

--- src/regex/Solution.java
+++ src/regex/Solution.java
@@ -1,4 +1,4 @@
-package dayXX;
+package regex;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
@@ -25,9 +24,9 @@ public class Solution {
 
         System.out.println("\nexecuting part 1");
         Instant start1 = Instant.now();
-        System.out.format(ANSI_BOLD + "result of part 1: %s\n" + ANSI_RESET, solver.part1(stringData));
+        System.out.format(ANSI_BOLD + "result of part 1: %s\n", solver.part1(stringData) + ANSI_RESET);
         Instant end1 = Instant.now();
-        System.out.println("Time taken: " + Duration.between(start1, end1));
+        System.out.println("Time taken: " + Duration.between(start1, end1) + "\n");
 
         System.out.println("\nexecuting part 2");
         Instant start2 = Instant.now();
@@ -51,19 +50,19 @@ public class Solution {
 
 class Solver {
     Object part1(String stringData) {
-        var lines = stringData.split(System.lineSeparator());
-        var data = Stream.of(lines).toList();
-        System.out.println(data);
-        var sum = data.stream()
-                .mapToInt(Integer::valueOf)
-                .sum();
-
         Pattern p = Pattern.compile("([\\d]+)");
-        var extractedInts = p.matcher(stringData).results().map(MatchResult::group)
-                .map(Integer::valueOf)
-                .collect(Collectors.toList());
+        var inventoryMatches = p.matcher(stringData);
+        inventoryMatches.results().map(MatchResult::group)
+                .mapToInt(Integer::valueOf)
+                .forEach(System.out::println);
+//        var lines = stringData.split(System.lineSeparator());
+//        var data = Stream.of(lines).toList();
+//        System.out.println(data);
+//        var sum = data.stream()
+//                .mapToInt(Integer::valueOf)
+//                .sum();
 
-        return sum;
+        return -1;
     }
 
     Object part2(String stringData) {
